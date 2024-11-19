61c2cff1b4298981f34be27e6e1be9582c4c9887
MinMyatPaing <minmyatpaing64@gmail.com>
2024-11-19 10:55:58 +0000

    Implemented demo dashboard

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFunctions, httpsCallable} from 'firebase/functions';
+import { getFunctions, httpsCallable } from "firebase/functions";
 
 // Production
-// Copy paste AI4SW Production Cofig here (get it from the firebase console)
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
